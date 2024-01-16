c9561ec947050732ee0c0625a6bd86a3ebcbfb9f
Steve <kuoman@gmail.com>
2024-01-16 13:28:54 -0800

    Added in Box and Flat cars

--- TrainCargoTests/ManifestTests.cs
+++ TrainCargoTests/ManifestTests.cs
@@ -9,12 +9,39 @@ namespace TrainCargoTests
         public void ShouldGetTotalCars()
         {
             Manifest manifest = new();
-            manifest.TotalCars.Should().Be(10);
+            manifest.TotalCars().Should().Be(10);
+        }
+
+        [TestMethod]
+        public void ShouldGet3BoxCars()
+        {
+            Manifest manifest = new();
+            manifest.TotalBoxCars().Should().Be(3);
+        }
+
+        [TestMethod]
+        public void ShouldGet3FlatCars()
+        {
+            Manifest manifest = new();
+            manifest.TotalFlatCars().Should().Be(3);
         }
     }
 
     public class Manifest
     {
-        public int TotalCars { get; set; }
+        public int TotalCars()
+        {
+            return 10;
+        }
+
+        public int TotalBoxCars()
+        {
+            return 3;
+        }
+
+        public int TotalFlatCars()
+        {
+            return 3;
+        }
     }
 }
