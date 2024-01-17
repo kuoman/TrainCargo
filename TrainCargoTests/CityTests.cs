6122e0a356316a01d2e39762cee68fe05043bf95
Steve <kuoman@gmail.com>
2024-01-17 12:39:18 -0800

    added in industry object

--- TrainCargoTests/CityTests.cs
+++ TrainCargoTests/CityTests.cs
@@ -1,7 +1,40 @@
-﻿namespace TrainCargoTests
+﻿using FluentAssertions;
+
+namespace TrainCargoTests
 {
     [TestClass]
     public class CityTests
     {
+        [TestMethod]
+        public void ShouldReturnListOfRollingStock()
+        {
+            City city = new City("a");
+
+            List<RollingStock> trainCars = city.GetCars();
+
+            trainCars.Should().HaveCount(3);
+        }
+    }
+
+    public class City
+    {
+        private readonly string _name;
+
+        public City(string name)
+        {
+            _name = name;
+        }
+
+        public List<RollingStock> GetCars()
+        {
+            List<RollingStock> rollingStocks = new List<RollingStock>
+            {
+                new FlatCar(_name, "1"),
+                new FreightCar(_name, "2"),
+                new TankerCar(_name, "3")
+            };
+
+            return rollingStocks;
+        }
     }
 }
---
+++ TrainCargoTests/IndustryTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace TrainCargoTests
+{
+    [TestClass]
+    public class IndustryTests
+    {
+        [TestMethod]
+        public void ShouldReturnRollingStock()
+        {
+            Industry industry = new Industry("1");
+
+            List<RollingStock> cars = industry.GetCars();
+
+            cars.Should().HaveCount(2);
+        }
+    }
+
+    public class Industry
+    {
+        private readonly string _industry;
+
+        public Industry(string industry)
+        {
+            _industry = industry;
+        }
+
+        public List<RollingStock> GetCars()
+        {
+            List<RollingStock> rollingStocks = new List<RollingStock>();
+            
+            rollingStocks.Add(new FlatCar("a", _industry));
+            rollingStocks.Add(new FlatCar("a", _industry));
+            
+            return rollingStocks;
+        }
+    }
+}
